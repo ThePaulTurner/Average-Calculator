4db2a85e984eaa91d676c12f1d5a0da68d3f442b
The_Paul_Turner <35648632+ThePaulTurner@users.noreply.github.com>
2021-01-21 23:45:52 -0800

    Add files via upload

---
+++ Instruction.cpp
@@ -0,0 +1,30 @@
+#include  <iostream>
+
+using namespace std;
+
+int main ()
+
+// Paul Turner CS265 C++
+
+{
+    // variable declaration
+        int num1;
+        int num2;
+        int num3;
+        int average;
+
+    // executable statements
+        num1 = 125;
+        num2 = 28;
+        num3 = -25;
+        average = (num1 + num2 + num3)/3;
+
+
+    // return statement
+        cout << "num1 = " << num1 << endl;
+        cout << "num2 = " << num2 << endl;
+        cout << "num3 = " << num3 << endl;
+        cout << "average = " << average << endl;
+
+  return 0;
+}
